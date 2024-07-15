8c560e4b388dde7e5e3fc9832acf2b3df67ca340
sonysarkis <sonygomez6@gmail.com>
2024-07-15 13:54:35 -0400

    feat: Agregar cálculo de permutaciones con repetición en la calculadora

---
+++ scriptPMRepetido.js
@@ -0,0 +1,34 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const calcResult = document.getElementById('calc-result');
+    const btnPermutate = document.getElementById('btn-permutate');
+    
+    function calculateExpression() {
+        // Obtén el valor de "n" y "k"
+        const n = parseInt(document.getElementById("n").value);
+        const kInput = document.getElementById("k").value;
+        const kArray = kInput.split(',').map(item => parseInt(item));
+      
+        // Calcula el factorial de un número
+        function factorial(num) {
+          if (num === 0 || num === 1) {
+            return 1;
+          }
+          return num * factorial(num - 1);
+        }
+      
+        // Calcula el factorial de n
+        const nFactorial = factorial(n);
+      
+        // Calcula el producto de los factoriales de k0, k1, ...
+        const productOfKFactorials = kArray.reduce((acc, k) => acc * factorial(k), 1);
+      
+        // Calcula el resultado
+        const result = nFactorial / productOfKFactorials;
+
+        calcResult.textContent = `Permutacion con repetición: ${result}`;
+    }
+
+    btnPermutate.addEventListener('click', function() {
+        calculateExpression();
+    });
+});
